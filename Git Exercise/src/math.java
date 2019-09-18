297184f9c5e4bb50c3ab9deec7fe781f6f724a4b
igat <matthewigat@gmail.com>
2019-09-17 20:47:21 -0700

    texting to see if my gitHub syncs

--- Git Exercise/src/math.java
+++ Git Exercise/src/math.java
@@ -11,37 +11,43 @@ public class math {
 		int num2 = sc.nextInt();
 		System.out.println("What math would you like to do?");
 		String s = sc.next();
-		
-		if(s.toLowerCase().contains("add")) {
-		System.out.println("The result is " + Addition(num1,num2));
-		}
-		else if(s.toLowerCase().contains("sub")) {
-			System.out.println("The result is " + Subtraction(num1,num2));
-			}
-		else if(s.toLowerCase().contains("mult")) {
-			System.out.println("The result is " + Multiplication(num1,num2));
-			}
-		else if(s.toLowerCase().contains("div")) {
-			System.out.println("The result is " + Division(num1,num2));
-		}
-		else {
+
+		if (s.toLowerCase().contains("add")) {
+			System.out.println("The result is " + Addition(num1, num2));
+		} else if (s.toLowerCase().contains("sub")) {
+			System.out.println("The result is " + Subtraction(num1, num2));
+		} else if (s.toLowerCase().contains("mult")) {
+			System.out.println("The result is " + Multiplication(num1, num2));
+		} else if (s.toLowerCase().contains("div")) {
+			System.out.println("The result is " + Division(num1, num2));
+		} else {
 			System.out.println("Sorry! We do not understand your command.");
 			System.out.println("hi");
+			
+			
+			
+			
+			
+			//igat's sample code
+			System.out.println("Testing to see if gitHub works.");
 		}
 		sc.close();
 	}
-	
+
 	public static double Addition(int num1, int num2) {
 		return 1;
 	}
+
 	public static double Subtraction(int num1, int num2) {
 		return 2;
 	}
+
 	public static double Multiplication(int num1, int num2) {
 		return 3;
 	}
+
 	public static double Division(int num1, int num2) {
 		return 4;
 	}
-	
-	}
+
+}
